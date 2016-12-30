5e875a80a99b47b914b1a90d5ba9ead288b2d283
Lukas Alexandre <lukas@codelogic.me>
2016-12-30 13:39:44 +0100

    Syntatical sugar

--- app/models/github/api.rb
+++ app/models/github/api.rb
@@ -28,14 +28,13 @@ module Github
     end
 
     def add_pull_request_comment(options)
-      client.create_pull_request_comment(
+      client.create_pull_request_comment \
         options[:commit].repo_name,
         options[:pull_request_number],
         options[:comment],
         options[:commit].sha,
         options[:filename],
         options[:patch_position]
-      )
     end
 
     def create_hook(full_repo_name, callback_endpoint)
@@ -81,61 +80,53 @@ module Github
     end
 
     def create_pending_status(full_repo_name, sha, description)
-      create_status(
+      create_status \
         repo: full_repo_name,
         sha: sha,
         state: "pending",
         description: description
-      )
     end
 
     def create_success_status(full_repo_name, sha, description)
-      create_status(
+      create_status \
         repo: full_repo_name,
         sha: sha,
         state: "success",
         description: description
-      )
     end
 
     def create_error_status(full_repo_name, sha, description, target_url = nil)
-      create_status(
+      create_status \
         repo: full_repo_name,
         sha: sha,
         state: "error",
         description: description,
         target_url: target_url
-      )
     end
 
     def add_collaborator(repo_name, username)
-      client.add_collaborator(
+      client.add_collaborator \
         repo_name,
         username,
-        accept: "application/vnd.github.ironman-preview+json",
-      )
+        accept: "application/vnd.github.ironman-preview+json"
     end
 
     def remove_collaborator(repo_name, username)
       # not sure if we need the accept header
-      client.remove_collaborator(
+      client.remove_collaborator \
         repo_name,
         username,
-        accept: "application/vnd.github.ironman-preview+json",
-      )
+        accept: "application/vnd.github.ironman-preview+json"
     end
 
     private
 
     def create_status(repo:, sha:, state:, description:, target_url: nil)
-      client.create_status(
-        repo,
-        sha,
-        state,
+      client.create_status \
+        repo, sha, state,
         context: "hound",
         description: description,
         target_url: target_url
-      )
     end
   end
 end
