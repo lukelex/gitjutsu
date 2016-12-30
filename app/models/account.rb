c310156fd039ded751046b71c3bc6a8c128869b7
Lukas Alexandre <lukas@codelogic.me>
2016-12-30 13:38:37 +0100

    Using ApplicationRecord as the parent AR class

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,4 @@
-class Account < ActiveRecord::Base
+class Account < ApplicationRecord
   has_one :user
   has_many :repositories
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   has_one :account
   has_many :repositories, through: :account
 
