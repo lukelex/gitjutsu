3f067fce832739be1a967ae4fc64b2730720a336
Lukas Alexandre <lukas@codelogic.me>
2017-05-29 13:43:37 -0300

    Adding secret to github's hook

--- app/models/github/hooks.rb
+++ app/models/github/hooks.rb
@@ -5,7 +5,7 @@ module Github
     def create(full_repo_name, callback_endpoint)
       api.create_hook \
         full_repo_name, "web",
-        { url: callback_endpoint, content_type: "json" },
+        { url: callback_endpoint, content_type: "json", secret: ENV["SECRET_TOKEN"] },
         { events: EVENTS, active: true }
     rescue Octokit::UnprocessableEntity => error
       if error.message.include? "Hook already exists"
