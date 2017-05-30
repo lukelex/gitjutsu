8e1a7114ba7aaeb273fdebd57a42ef4b4920e6fa
Lukas Alexandre <lukas@codelogic.me>
2017-05-29 22:27:38 -0300

    Enables debugging Octokit's requests to GitHub

---
+++ config/initializers/octokit.rb
@@ -0,0 +1,12 @@
+if ENV["DEBUG_OCTOKIT"]
+  require "octokit"
+  require "faraday"
+
+  # TODO: Find a way to log only the the request string
+  stack = Faraday::RackBuilder.new do |builder|
+    builder.response :logger
+    builder.use Octokit::Response::RaiseError
+    builder.adapter Faraday.default_adapter
+  end
+  Octokit.middleware = stack
+end
