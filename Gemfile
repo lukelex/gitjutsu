7138febce9dd52f0cb5316a6cb09b67d10174316
Lukas Alexandre <lukas@codelogic.me>
2018-08-18 15:34:49 +0200

    Updating Rails to 5.2.1

--- Gemfile
+++ Gemfile
@@ -3,29 +3,28 @@
 source "https://rubygems.org"
 ruby "2.5.1"
 
+gem "closed_struct"
+gem "dotenv-rails"
 gem "pg", "~> 0.18"
 gem "puma", "~> 3.0"
-gem "rails", "~> 5.1.1"
-gem "redis-rails", "~> 5.0"
+gem "rails", "~> 5.2.1"
+gem "slim"
 
+# Service integration
+gem "octokit"
+gem "omniauth-github"
+
+# Cache & Queueing
+gem "redis-rails", "~> 5.0"
+gem "resque", "~> 1.27"
+
+# Frontend tools
 gem "bootstrap-sass", "~> 3.3"
 gem "font-awesome-rails", "~> 4.7"
 gem "jquery-rails", "~> 4.3"
 gem "sass-rails", "~> 5.0"
-gem "uglifier", ">= 1.3.0"
-
 gem "turbolinks", "~> 5"
-# Use Redis adapter to run Action Cable in production
-# gem "redis", "~> 3.0"
-
-gem "dotenv-rails"
-gem "octokit"
-gem "omniauth-github"
-gem "resque", "~> 1.27"
-gem "slim"
-
-# utilities
-gem "closed_struct"
+gem "uglifier", ">= 1.3.0"
 
 group :development, :test do
   gem "better_errors"
@@ -39,4 +38,5 @@ end
 group :development do
   gem "guard", require: false
   gem "guard-rspec", require: false
+  gem "bootsnap", require: false
 end
--- config/application.rb
+++ config/application.rb
@@ -8,19 +8,15 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-
 module Gitjutsu
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
     config.action_controller.include_all_helpers = false
-
-    config.autoload_paths << "#{Rails.root}/models/github/"
-
-    config.active_job.queue_adapter = :resque
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
